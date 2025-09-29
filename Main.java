a9f8f035f909a1101aecb196e28be04e30a617cc
Jennifer087 <jenniferhuynh087@gmail.com>
2025-09-29 18:20:14 +0000

    ;C

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main
   public static void main(String[] args)
   {
     // write your code here
+    lastFirstN("cream", "butter", 3);
+
+    stringManip ("cream", "butter");
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +13,42 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+  String output;
+  String firstN;
+  String lastN;
+
+ //first N letters of s2
+firstN = s2.substring (0,n);
+
+//last N letters of s1
+int startPosition = s1.length () - n;
+lastN = s1.substring (startPosition);
+
+
+  output = lastN + firstN;
+  print.out.println(output);
+
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
+  String Upper;
+  String Lower;
+
+  Int LengthOfupper = s1.length();
+  Upper = s1.toUpperCase();
+
+   
+  Lower = s2.substring(0) ;
+  Lower = s2.toLowerCase ();
+
+  System.out.println (Upper + Lower);
+
   
+
+
   }
 
   /** Precondition: s1 and s2 are not null
